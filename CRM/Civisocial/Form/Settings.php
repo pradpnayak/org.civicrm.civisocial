305a4be188b502978e7f78ba8bab516426ba018e
Dilip Raj Baral <rajb.dilip@gmail.com>
2016-06-01 15:27:13 +0530

    Refactor Settings form code

--- CRM/Civisocial/Form/Settings.php
+++ CRM/Civisocial/Form/Settings.php
@@ -3,119 +3,50 @@
 require_once 'CRM/Core/Form.php';
 
 /**
- * Form controller class
+ * Civisocial Settings Form controller class
  *
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
-class CRM_Civisocial_Form_Settings extends CRM_Core_Form {
-	private $_settingFilter = array('group' => 'civisocial');
+class CRM_Civisocial_Form_Settings extends CRM_Admin_Form_Setting {
+	protected $_settings = array(
+		'enable_facebook' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,  
+		'facebook_app_id' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,    
+		'facebook_secret' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,           
+		'enable_googleplus' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+		'google_plus_key' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,     
+		'google_plus_secret' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+		'enable_twitter' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+		'twitter_consumer_key' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+		'twitter_consumer_secret' => CRM_Core_BAO_Setting::SYSTEM_PREFERENCES_NAME,
+	);
 
-	private $_submittedValues = array();
-	private $_settings = array();
+  /**
+   * Process the form submission.
+   */
+	public function preProcess() {
+		CRM_Utils_System::setTitle(ts('CiviSocial OAuth Credential Preferences')); 
+		parent::preProcess();   
+	}
 
 	/**
-     * Build the settings form
-     */
+	 * Build the settings form
+	 */
 	public function buildQuickForm() {
-		CRM_Utils_System::setTitle(ts('CiviSocial OAuth Credential Preferences'));
-
-		$settings = $this->getFormSettings();
-		foreach ($settings as $name => $setting) {
-			if (isset($setting['quick_form_type'])) {
-				$add = 'add' . $setting['quick_form_type'];
-				if ($add == 'addElement') {
-					$this->$add($setting['html_type'], $name, ts($setting['title']), CRM_Utils_Array::value('html_attributes', $setting, array()));
-				} else {
-					$this->$add($name, ts($setting['title']));
-				}
-				$this->assign("{$setting['description']}_description", ts('description'));
-			}
-		}
-		$this->addButtons(array(
-	        array(
-	          'type' => 'submit',
-	          'name' => ts('Save'),
-	          'isDefault' => TRUE,
-	        ),
-	        array(
-	          'type' => 'cancel',
-	          'name' => ts('Cancel'),
-	        ),
-	      )
-	    );
-		// export form elements
-		$this->assign('elementNames', $this->getRenderableElementNames());
+		$formFields = array(
+			ts('Facebook') => array('enable_facebook', 'facebook_app_id', 'facebook_secret'),
+			ts('Google') => array('enable_googleplus', 'google_plus_key', 'google_plus_secret'),
+			ts('Twitter') => array('enable_twitter', 'twitter_consumer_key', 'twitter_consumer_secret'),
+		);
+		$this->assign('formFields', $formFields);
 		parent::buildQuickForm();
 	}
 
 	/**
-   	 * Process the form submission.
-   	 */
+	 * Process the form submission.
+	 */
 	public function postProcess() {
-		$this->_submittedValues = $this->exportValues();
-		if ($this->saveSettings()) {
-			CRM_Core_Session::setStatus(ts('CiviSocial preferences have been saved.'), ts('Saved'), 'success');
-		}
-	}
-
-	/**
-	 * Get the fields/elements defined in this form.
-	 *
-	 * @return array (string)
-	 */
-	public function getRenderableElementNames() {
-		// The _elements list includes some items which should not be
-		// auto-rendered in the loop -- such as "qfKey" and "buttons". These
-		// items don't have labels. We'll identify renderable by filtering on
-		// the 'label'.
-		$elementNames = array();
-		foreach ($this->_elements as $element) {
-			$label = $element->getLabel();
-			if (!empty($label)) {
-				$elementNames[] = $element->getName();
-			}
-		}
-		return $elementNames;
-	}
-
-	/**
-	 * Get the settings we are going to allow to be set on this form.
-	 *
-	 * @return array
-	 */
-	public function getFormSettings() {
-		if (empty($this->_settings)) {
-			$settings = civicrm_api3('setting', 'getfields', array('filters' => $this->_settingFilter));
-		}
-		// $extraSettings = civicrm_api3('setting', 'getfields', array('filters' => array('group' => 'accountsync')));
-		// $settings = $settings['values'] + $extraSettings['values'];
-		return $settings['values'];
-	}
-
-	/**
-	 * Get the settings we are going to allow to be set on this form.
-	 *
-	 * @return array
-	 */
-	public function saveSettings() {
-		$settings = $this->getFormSettings();
-		$values = array_intersect_key($this->_submittedValues, $settings);
-		return (civicrm_api3('setting', 'create', $values));
-	}
-
-	/**
-	 * Set defaults for form.
-	 *
-	 * @see CRM_Core_Form::setDefaultValues()
-	 */
-	public function setDefaultValues() {
-		$existing = civicrm_api3('setting', 'get', array('return' => array_keys($this->getFormSettings())));
-		$defaults = array();
-		$domainID = CRM_Core_Config::domainID();
-		foreach ($existing['values'][$domainID] as $name => $value) {
-			$defaults[$name] = $value;
-		}
-		return $defaults;
+		parent::postProcess();
+		CRM_Core_Session::setStatus(ts('CiviSocial preferences have been saved.'), ts('Saved'), 'success');
 	}
 
 }
